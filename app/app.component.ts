b0899e0b8684ea5cc1b900d9a4e8147b4d42226a
naveen kumar <naveen.aitian@gmail.com>
2016-05-18 22:17:50 +0530

    First commit for the angular 2 quick start app.

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by naveen_kumar11 on 18/05/16.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'my-app',
+    template:'<h1>My first angular app</h1>'
+})
+
+export class AppComponent {}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by naveen_kumar11 on 18/05/16.
+ */
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
